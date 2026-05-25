94844b89cdeb16348721e9d92300b8392473225e
namans895 <s895naman@gmail.com>
2026-05-26 01:02:33 +0530

    Create calculator.py

---
+++ src/cli_calculator/tests/src/cli_calculator/calculator.py
@@ -0,0 +1,7 @@
+from decimal import Decimal, DivisionByZero, InvalidOperation, getcontext
+from typing import Callable
+from typing import Callable, Union
+
+
+Number = int | Decimal
+Number = Union[int, Decimal]
